6cfae4b9f831a4afc8f5cd63319f959a1aa6c691
Ryan Ardray <rardray@gmail.com>
2019-01-31 07:39:47 -0600

    connection to mlab for deployment

--- server/config/main.js
+++ server/config/main.js
@@ -1,7 +1,7 @@
 module.exports = {
-    'secret': 'secret-catchphrase',
-    'database': 'mongodb://localhost:27017/imageslider',
-    'port': process.env.PORT || 3001
-}
+  secret: "secret-catchphrase",
+  database: process.env.MONGOLAB_URI,
+  port: process.env.PORT || 3001
+};
 
 //configuration options could be placed in index.js database could be put directly in mongoose.on
